7c653c65ab7814ede170d165c4983ec4e7815954
anderson.antunes <anderson_oliveira.unochapeco.edu.br>
2026-03-25 17:05:22 -0300

    feat: ajustado conflitos

--- jogaeEsports/src/routes/index.routes.tsx
+++ jogaeEsports/src/routes/index.routes.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Login from "../pages/login";
 import CidadeEsportes from "../pages/cidadeEsportes";
-import Home from "../pages/home";
-import Jogos from "../pages/jogos";
-import NewJogos from "../pages/jogos/newJogo";
-import Explorar from "../pages/explorar";
-import Perfil from "../pages/perfil";
-import CustomTabBar from "../componets/CustomTabBar";
 import Home from "../pages/home";
 import Jogos from "../pages/jogos";
 import NewJogos from "../pages/jogos/newJogo";
@@ -34,7 +25,6 @@ export default function Routes() {
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="CidadeEsportes" component={CidadeEsportes} />
             <Stack.Screen name="Main" component={MainTabs} />
-            <Stack.Screen name="Main" component={MainTabs} />
         </Stack.Navigator>
     );
 }
@@ -51,23 +41,6 @@ function MainTabs() {
             <Tab.Screen name="Explorar" component={Explorar} />
             <Tab.Screen name="Criar" component={NewJogos} />
             <Tab.Screen name="Jogos" component={Jogos} />
-            <Tab.Screen name="Perfil" component={Perfil} />
-        </Tab.Navigator>
-    );
-}
-
-function MainTabs() {
-    const Tab = createBottomTabNavigator();
-
-    return (
-        <Tab.Navigator
-            screenOptions={{ headerShown: false }}
-            tabBar={(props) => <CustomTabBar {...props} />}
-        >
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Jogos" component={Jogos} />
-            <Tab.Screen name="Criar" component={NewJogos} />
-            <Tab.Screen name="Explorar" component={Explorar} />
             <Tab.Screen name="Perfil" component={Perfil} />
         </Tab.Navigator>
     );
